5e756a3fbf51d822adaee626cff3b27c989fa8a1
Carlos Coves Prieto <escorponox@gmail.com>
2017-01-23 19:20:59 +0100

    htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,48 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = env => {
+
+  const rules = [
+    {
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: {
+        loader: 'babel-loader',
+        options: {presets: ['es2015']}
+      }
+    }
+  ];
+
+  env.dev
+    ? rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader?sourceMap',
+        'sass-loader?sourceMap',
+      ]
+    })
+    : rules.push({
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+    });
+
+  const plugins = [
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body',
+    })
+  ];
+
+  if (env.prod) {
+    plugins.push(new ExtractTextPlugin({filename: '[name].bundle.css', disable: false, allChunks: true}))
+  }
+
   return {
     context: path.resolve(__dirname, './app'),
     devtool: env.prod ? 'source-map' : 'eval',
@@ -19,36 +59,9 @@ module.exports = env => {
       publicPath: '/'
     },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: [/node_modules/],
-          use: {
-            loader: 'babel-loader',
-            options: {presets: ['es2015']}
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader',
-            'css-loader?sourceMap',
-            'sass-loader?sourceMap',
-          ]
-        }]
+      rules: rules
     },
-    plugins: [
-      new ExtractTextPlugin({filename: '[name].bundle.css', disable: false, allChunks: true}),
-      // new webpack.optimize.CommonsChunkPlugin({
-      //   name: 'commons',
-      //   filename: 'commons.js',
-      //   minChunks: 2,
-      // })
-      // new webpack.HotModuleReplacementPlugin(),
-      // enable HMR globally
-      //
-      new webpack.NamedModulesPlugin(),
-    ]
+    plugins: plugins
   }
 }
 ;
